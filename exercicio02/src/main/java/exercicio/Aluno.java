cd472e8fb8e1532b3b6d1c0595febf3847baf9d6
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-04 20:39:04 -0300

    Criei a classe Aluno

---
+++ exercicio02/src/main/java/exercicio/Aluno.java
@@ -0,0 +1,18 @@
+
+package exercicio;
+
+
+public class Aluno {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    
+    Aluno(String nome, int codigo, String endereco){
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+    public String verNome(){
+        return this.nome;
+    }
+}
