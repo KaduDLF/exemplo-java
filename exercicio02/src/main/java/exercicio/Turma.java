4f0c35e10054293fde831dd1cc70a8f06c9a5e13
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-04 20:45:59 -0300

    Criei a classe Turma.java

---
+++ exercicio02/src/main/java/exercicio/Turma.java
@@ -0,0 +1,12 @@
+
+package exercicio;
+
+public class Turma {
+   private String descricao;
+    private Curso curso;
+    
+    Turma(String descricao, Curso curso){
+        this.descricao = descricao;
+        this.curso = curso;
+    } 
+}
