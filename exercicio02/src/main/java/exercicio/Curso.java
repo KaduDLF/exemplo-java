3d2606e90ad3db9ba65541832e0fd890dcda9c14
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-04 20:40:18 -0300

    criei a classe Curso

---
+++ exercicio02/src/main/java/exercicio/Curso.java
@@ -0,0 +1,13 @@
+
+package exercicio;
+
+
+public class Curso {
+    private String nome;
+    private int duracao;
+    
+    Curso(String nome, int duracao){
+        this.nome = nome;
+        this.duracao = duracao;
+    }
+}
