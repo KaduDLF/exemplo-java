053d3d20abfb8b7b3118d84e822ad30a1c0ff930
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-04 20:49:10 -0300

    alterei a classe App para testar tudo

--- exercicio02/src/main/java/exercicio/App.java
+++ exercicio02/src/main/java/exercicio/App.java
@@ -4,6 +4,11 @@ package exercicio;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        Aluno a1 = new Aluno("Carlos", 19, "rua 63 qd 5 ltp 21");
+        Curso curso1 = new Curso("Sistemas", 4);
+        Turma turma1 = new Turma("1 periodo", curso1);
+        Matricula matricula1 = new Matricula(98, a1, turma1);
+        
+        System.out.println("a matricula do aluno: " + matricula1.verAluno().verNome() + " é " + matricula1.verNumero());
     }
 }
