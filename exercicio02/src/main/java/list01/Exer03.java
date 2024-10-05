21ccdd6fe6de2adcdf09cb36b29a219619076aea
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-05 20:47:03 -0300

    resolvendo a primeira alternativa

--- exercicio02/src/main/java/list01/Exer03.java
+++ exercicio02/src/main/java/list01/Exer03.java
@@ -1,8 +1,15 @@
 
 package list01;
 
+import java.util.Scanner;
+
 public class Exer03 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome;
+        System.out.println("Digite seu nome completo");
+        nome = scan.nextLine(); // armazena o nome
         
+        System.out.println("A quantidade total de letras é de: " + nome.length());
     }
 }
