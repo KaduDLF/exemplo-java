2669d17d982fe5a88d5acb2f8481f479a1d2987d
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-05 21:10:58 -0300

    Resolvendo a segunda alternativa

--- exercicio02/src/main/java/list01/Exer03.java
+++ exercicio02/src/main/java/list01/Exer03.java
@@ -7,9 +7,20 @@ public class Exer03 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String nome;
+        int qtdLetras; // variavel que armazena a quantidade de Letras
+        int qtdVogais; // variavel que armazena a quantidade de vogais
         System.out.println("Digite seu nome completo");
         nome = scan.nextLine(); // armazena o nome
         nome = nome.replace(" ", ""); // substituindo espaço por 'vazio' para não impactar na contagem de letras
-        System.out.println("A quantidade total de letras é de: " + nome.length());
+        qtdLetras = nome.length();
+        System.out.println("A quantidade total de letras é de: " + qtdLetras);
+        nome = nome.toLowerCase();
+        nome = nome.replace("a", "");
+        nome = nome.replace("e", "");
+        nome = nome.replace("i", "");
+        nome = nome.replace("o", "");
+        nome = nome.replace("u", "");
+        qtdVogais = qtdLetras - nome.length();
+        System.out.println("A quantidade de vogais é de: " + qtdVogais);
     }
 }
