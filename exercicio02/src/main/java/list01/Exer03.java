e0b1a10a56fc6b4dd60731ae78add9db9b3c15b3
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-05 21:47:40 -0300

    Adicionando nova forma de resolver usando estruturas de repetição e condicional

--- exercicio02/src/main/java/list01/Exer03.java
+++ exercicio02/src/main/java/list01/Exer03.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 public class Exer03 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String nome;
+        String nome, nome2;
         int qtdLetras; // variavel que armazena a quantidade de Letras
         int qtdVogais; // variavel que armazena a quantidade de vogais
         System.out.println("Digite seu nome completo");
         nome = scan.nextLine(); // armazena o nome
+        nome2 = nome;
         nome = nome.replace(" ", ""); // substituindo espaço por 'vazio' para não impactar na contagem de letras
         qtdLetras = nome.length();
         System.out.println("A quantidade total de letras é de: " + qtdLetras);
@@ -22,7 +23,33 @@ public class Exer03 {
         nome = nome.replace("u", "");
         qtdVogais = qtdLetras - nome.length();
         System.out.println("A quantidade de vogais é de: " + qtdVogais);
-        System.out.println("A quantidade de vogais é de: " + (qtdLetras - qtdVogais));
+        System.out.println("A quantidade de Consoantes é de: " + (qtdLetras - qtdVogais));
+        // esse codigo de cima, tira todas as vogais do nome e subtrai as letras totais pelas total de letra sem vogal
+        // Depois ele subtrai o total de letras, menos o total de vogais e descobre o resultado de Consoantes
+        
+        // agora vou fazer usando estrutura de repetição e condicional
+        
+        System.out.println(" ------------------------------ ");
+        char vazio;
+        int qtdVazio = 0;
+        for(int i = 0;i < nome2.length(); i++){
+            vazio = nome2.charAt(i);
+            if(vazio == ' '){
+                qtdVazio++;
+            }
+        }
+        int nome2SemEspaco = nome2.length() - qtdVazio;
+        System.out.println("A quantidade de Letras é de: " + nome2SemEspaco);
+        char vogal; 
+        int qtdVogal = 0;
+        for(int i = 0; i< nome2.length(); i++){
+            vogal = nome2.charAt(i);
+            if(vogal == 'a' || vogal == 'e' || vogal == 'i' || vogal == 'o' || vogal == 'u'){
+                qtdVogal++;                    
+            }
+        }
+        System.out.println("A quantidade de Vogal é de: " + qtdVogal);
+        System.out.println("A quantidade de consoante é de: " + (nome2SemEspaco - qtdVogal));
         
     }
 }
