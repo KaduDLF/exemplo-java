330fae9c33e0ed48ed85c3cea49538ab47c8a3d4
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2024-10-12 20:25:58 -0300

    Teste

---
+++ exercicio02/src/main/java/list02/Test.java
@@ -0,0 +1,11 @@
+package list02;
+
+public class Test {
+    String nome;
+    int idade;
+    
+    Test(){
+        this.nome = "jozo";
+        this.idade = 12;
+    }
+}
---
+++ exercicio02/src/main/java/list02/Teste.java
@@ -0,0 +1,9 @@
+package list02;
+
+public class Teste {
+    public static void main(String[] args) {
+        Test tst = new Test();
+        System.out.println(tst.nome);
+        System.out.println(tst.idade);
+    }
+}
